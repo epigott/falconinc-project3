826aac8223939bc672317691df10652ea2905c95
Eric Pigott <alphatauri8@gmail.com>
2020-02-20 14:34:29 -0500

    Basic JFrame Created

--- Eric's falconinc-project3/src/falconinc/SearchEngine.java
+++ Eric's falconinc-project3/src/falconinc/SearchEngine.java
@@ -5,12 +5,27 @@
  */
 package falconinc;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
 /**
  *
  * @author epigott
  */
 public class SearchEngine {
+    
+    
+    
     public static void main (String[] args) {
-        System.out.println("First Code");
+    //    System.out.println("First Code");
+    
+    JFrame sewindow;
+    
+    sewindow = new JFrame("Search Engine - Falcon INC");
+    
+    sewindow.setSize(800, 600);
+    sewindow.setVisible(true);
     }
+    
 }
