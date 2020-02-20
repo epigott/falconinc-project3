f04144074a9c286b733a654240dffcd83eb692e2
Eric Pigott <alphatauri8@gmail.com>
2020-02-20 15:59:50 -0500

    Edit of Frame, Add Heading

--- Eric's falconinc-project3/src/falconinc/SearchEngine.java
+++ Eric's falconinc-project3/src/falconinc/SearchEngine.java
@@ -2,6 +2,7 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
+ * NOTE: Code is not considered 'clean', 'tidy', or 'complete', until explicitly indicated here or submitted as final code.
  */
 package falconinc;
 
@@ -16,14 +17,31 @@ import java.awt.event.*;
 public class SearchEngine {
     
     public static void main (String[] args) {
-    //    System.out.println("First Code");
     
     JFrame sewindow;
     
     sewindow = new JFrame("Search Engine - Falcon INC");
     
-    sewindow.setSize(800, 600);
-    sewindow.setVisible(true);
-    }
+    // Will prevent the system process created by running in IDE from hanging when closed.
+    sewindow.addWindowListener(new WindowAdapter() {
+        @Override
+        public void windowClosing( WindowEvent we ) {
+           System.out.println("Have A Good Day!");
+           System.exit(0);
+           }
+        }
+      );
     
+    
+    sewindow.setSize(800, 600);
+    
+    // Create and add a Text Heading.
+    JLabel heading = new JLabel("Falcon INC Search Engine", JLabel.CENTER);
+    // Font heading_font = new Font(); // In Progress;
+    sewindow.add(heading);
+    
+    sewindow.setVisible(true);
+    
+    }
+        
 }
