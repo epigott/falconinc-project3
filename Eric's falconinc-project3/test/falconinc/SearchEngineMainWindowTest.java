a40d529360bd5d2cc34b9a5802df23d31eb0251c
Eric Pigott <alphatauri8@gmail.com>
2020-02-25 13:50:23 -0500

    Added Test Classes (Better)

---
+++ Eric's falconinc-project3/test/falconinc/SearchEngineMainWindowTest.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package falconinc;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author epigott
+ */
+public class SearchEngineMainWindowTest {
+    
+    public SearchEngineMainWindowTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of main method, of class SearchEngineMainWindow.
+     */
+    @Test
+    public void testMain() {
+        System.out.println("main");
+        String[] args = null;
+        SearchEngineMainWindow.main(args);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
