cd2c342264f9911ce85159aaee614edfb8b19eb2
Eric Pigott <alphatauri8@gmail.com>
2020-02-25 13:08:47 -0500

    Added Test Class

---
+++ Eric's falconinc-project3/test/falconinc/TestRunner.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package falconinc;
+
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+
+
+public class TestRunner {
+   public static void main(String[] args) {
+      		
+     System.out.println("Success!");
+   }
+} 
