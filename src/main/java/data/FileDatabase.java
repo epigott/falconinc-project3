6f2dbf83ca390666861b78bb56a944aca0fa4621
bclou <bclou@DESKTOP-HLTDABP.home>
2020-02-28 14:49:09 -0500

    maybe this

--- src/main/java/data/FileDatabase.java
+++ src/main/java/data/FileDatabase.java
@@ -5,8 +5,11 @@ import java.util.*;
 
 public class FileDatabase {
 	static private Connection con;
+	static String[] columns;
+	static String[] dataType;
+	static String tableName;
 
-	public static void getConnection() throws ClassNotFoundException, SQLException {		
+	public static void getConnection() throws ClassNotFoundException, SQLException {
 		Class.forName("org.sqlite.JDBC");
 		con = DriverManager.getConnection("jdbc:sqlite:TheFilePile.db");
 		initialize();		
@@ -14,6 +17,33 @@ public class FileDatabase {
 
 
 	static private void initialize() throws SQLException {	
+		columns = new String[] {"id", "fileName"};
+		dataType = new String[] {"integer", "varchar(50)"};
+		tableName = "thePile";
+		
+		/*SQLite uses a database to store its databases called sqlite_master.
+		 * Here where looking in that database to find thePile.
+		 */
+		//"SELECT name FROM sqlite_master WHERE type='table' AND name='thePile'"
+		String sql = ("SELECT name FROM sqlite_master WHERE type='table' AND name='" + tableName + "'");
+		Statement state = con.createStatement();
+		ResultSet res = state.executeQuery(sql);
+		
+		//If the ResultSet comes back empty we create a table.
+		if( !res.next()){				
+			//sql CREATE TABLE thePile(id integer, fileName varchar(50), primary key(id);
+			sql = "CREATE TABLE " + tableName + "(";
+			for(int x = 0 ; x < columns.length ; ++x) {				
+				sql += " ";
+				sql += columns[x];
+				sql += " ";
+				sql +=  dataType[x] + ",";
+			}
+			sql += " primary key(" + columns[0] + "));";
+			
+			Statement state2 = con.createStatement();
+			state2.execute(sql);
+		}
 		
 	}
 
