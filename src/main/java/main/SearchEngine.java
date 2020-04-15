257927d1f9f9e2d75d8e4d7cca24d2c0afc2a2b8
Akanksha Chavan <akankshachavan92@gmail.com>
2020-04-15 17:50:22 -0400

    update line 72

--- src/main/java/main/SearchEngine.java
+++ src/main/java/main/SearchEngine.java
@@ -69,6 +69,7 @@ public class SearchEngine {
 			// start of for loop for search query, check for valid fileID
 			for(int x = 1; x < query.size(); x++) {
 				
+				index += query.get(x);
 			}
 			// try-catch SQL exception for checking valid queries
 			try {
